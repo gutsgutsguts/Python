62a841d84ea2816fb086c1391ee29dba0356ecf3
seonhong <cowaboongga@gmail.com>
2023-11-08 11:37:09 +0900

    11.08 python commit

--- main.py
+++ main.py
@@ -8,15 +8,71 @@ options = Options()
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-browser = webdriver.Chrome(options=options)
+def get_page_count(keyword):
+    base_url = "https://kr.indeed.com/jobs?q="
+    serch_term = "python"
 
-base_url = "https://kr.indeed.com/jobs?q="
+    browser = webdriver.Chrome(options=options)
 
-browser.get("https://weworkremotely.com/remote-jobs/search?term=python")
+    response = browser.get(f"{base_url}{keyword}")
+    
+    if response.status_code != 200:
+        print("Cant request page")
+    else:
+        soup = BeautifulSoup(response.text, "html.parser")
+        pagination = soup.find("ul", class_="pagiantion-list")
+        if pagination == None:
+            return 1
+
+        pages = pagination.find_all("li", recursive=False)
+        count = len(pages)
+
+        if count >= 5:
+            return 5
+        else:
+            return count 
+
+print(get_page_count("python"))
 
 
-print(browser.page_source)
 
+def extract_indeed_jobs(keyword):
+    pages = get_page_count(keyword)
+    for page in range(pages):
+        base_url = "https://kr.indeed.com/jobs?q="
+        serch_term = "python"
 
-while(True):
-    pass
+        browser = webdriver.Chrome(options=options)
+
+        response = browser.get(f"{base_url}{keyword}")
+        
+        if response.status_code != 200:
+            print("Cant request page")
+        else:
+
+            soup = BeautifulSoup(browser.page_source, "html.parser")
+            job_list = soup.find("ul", class_="jobsearch-ResultsList")
+
+            jobs = job_list.find_all('li', recursive=False)
+
+            results = []
+
+            for job in jobs:
+                zone = job.find("div", class_="mosaic-zone")
+                if zone == None:
+                    anchor = job.select_one("h2 a")
+                    company = job.find("span", class_="companyName")
+                    location = job.find("div", class_="companyLocation")
+
+                    job_data = {
+                    'link' : f"https://kr.indeed.com{link}",
+                    'company' : company.string,
+                    'location' : location.string,
+                    'position' : title
+                    
+                }
+
+            results.append(job_data)
+
+    for result in results:
+        print(result, "///////\n/////////")
